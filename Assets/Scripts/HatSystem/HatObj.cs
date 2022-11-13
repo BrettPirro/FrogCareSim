dc86fc4e714e2fe75c845b70093a14f9747423f7
MrAporia <114781092+BrettPirro@users.noreply.github.com>
2022-11-12 19:53:41 -0500

    UI Patching

---
+++ Assets/Scripts/HatSystem/HatObj.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class HatObj : ScriptableObject
+{
+    string HatName;
+    Sprite sprite;
+    int Price;
+
+
+
+
+    
+}
